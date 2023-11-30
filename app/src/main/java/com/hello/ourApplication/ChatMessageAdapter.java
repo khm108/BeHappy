7b08a28b16b642d4b2f100737ef28e3f7e9f2bd8
이주애 <joojuae02@gachon.ac.kr>
2023-11-30 12:08:27 +0900

    gpt채팅기능 구현
    key 마스킹 해둬서 해당부분은 따로 너어주셔야 합니다!!! 나중에 푸시할때도 똑같이 마스킹해서 넣어주세요

--- app/src/main/java/com/hello/ourApplication/ChatMessageAdapter.java
+++ app/src/main/java/com/hello/ourApplication/ChatMessageAdapter.java
@@ -3,6 +3,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.Message;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,69 +14,59 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
-public class ChatMessageAdapter extends ArrayAdapter {
-    private TextView chatText;
-    private List chatMessageList = new ArrayList<ChatMessage>();
-    private LinearLayout singleMessageContainer;
 
-    // ChatMessage 객체를 어댑터에 추가
-    public void add(@Nullable ChatMessage object) {
-        chatMessageList.add(object);
-        super.add(object);
+public class ChatMessageAdapter extends RecyclerView.Adapter<ChatMessageAdapter.ViewHolder> {
+
+    List<ChatMessage> messageList;
+
+    public ChatMessageAdapter(List<ChatMessage> messageList) {
+        this.messageList = messageList;
     }
 
-    // 생성자
-    public ChatMessageAdapter(@NonNull Context context, int textViewResourceId) {
-        super(context, textViewResourceId);
-    }
-
-    // 어댑터에 있는 채팅 메시지의 개수 반환
-    @Override
-    public int getCount() {
-        return this.chatMessageList.size();
-    }
-
-    // 지정된 인덱스에 있는 채팅 메시지 객체 반환
-    @Nullable
-    @Override
-    public ChatMessage getItem(int index) {
-        return (ChatMessage) this.chatMessageList.get(index);
-    }
-
-    // 채팅 메시지를 표시하는 뷰 생성 및 설정
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
-        View row = convertView;
-        if (row == null) {
-            // 새로운 행 뷰를 생성
-            LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            row = inflater.inflate(R.layout.chatting_message, parent, false);
-        }
-
-        singleMessageContainer = (LinearLayout) row.findViewById(R.id.singleMessageContainer);
-        ChatMessage chatMessageObj = getItem(position);
-        chatText = (TextView) row.findViewById(R.id.singleMessage);
-        chatText.setText(chatMessageObj.message);
-
-        //left -> true: GPT, false: user
-        // 채팅 메시지의 왼쪽 또는 오른쪽에 배경 이미지 설정
-        chatText.setBackgroundResource(chatMessageObj.left ? R.drawable.bubble_a : R.drawable.bubble_b);
-
-        // 채팅 메시지의 텍스트 정렬을 왼쪽 또는 오른쪽으로 설정
-        singleMessageContainer.setGravity(chatMessageObj.left ? Gravity.LEFT : Gravity.RIGHT);
-        return row;
-
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.chatting_message, null);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
-    // 바이트 배열을 비트맵으로 디코딩하는 메서드
-    public Bitmap decodeToBitmap(byte[] decodedByte) {
-        return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length);
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ChatMessage message = messageList.get(position);
+        if(message.getSentBy().equals(ChatMessage.SENT_BY_ME)){
+            holder.left_chat_view.setVisibility(View.GONE);
+            holder.right_chat_view.setVisibility(View.VISIBLE);
+            holder.right_chat_tv.setText(message.getMessage());
+        } else {
+            holder.right_chat_view.setVisibility(View.GONE);
+            holder.left_chat_view.setVisibility(View.VISIBLE);
+            holder.left_chat_tv.setText(message.getMessage());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        LinearLayout left_chat_view, right_chat_view;
+        TextView left_chat_tv, right_chat_tv;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            left_chat_view = itemView.findViewById(R.id.left_chat_view);
+            right_chat_view = itemView.findViewById(R.id.right_chat_view);
+            left_chat_tv = itemView.findViewById(R.id.left_chat_tv);
+            right_chat_tv = itemView.findViewById(R.id.right_chat_tv);
+        }
     }
 }
+
